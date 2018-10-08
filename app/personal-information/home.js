4c3f9c7ac65ce17b26dc2f0fa3996106eb6ecea0
Ryan <18310589843@163.com>
2018-10-08 18:56:27 +0800

    我的页面

---
+++ app/personal-information/home.js
@@ -0,0 +1,207 @@
+import React,{Component} from 'react'
+import {ScrollView,View,Text,StyleSheet,Image,} from 'react-native'
+
+export default class Information extends Component{
+  render(){
+    return (
+      <ScrollView>
+      <View style={styles.container}>
+        <Image source={require('../image/bgimage.jpg')}
+               style={{width:'100%',height:300}}/>
+       <View style={styles.top}>
+         <View style={styles.header}>
+           <Image source={require('../image/touxiang.jpeg')}
+                  style={{width:80,height:80,borderRadius:50,borderWidth:3,borderColor:'#fff'}}/>
+           <View style={styles.headerName}>
+             <Text style={{fontSize:15,color:'#fff',fontWeight: 'bold',}}>Ryan梅子</Text>
+             <Image source={require('../image/boy.png')}
+                    style={{width:20,height:20,marginLeft: 20,}}/>
+           </View>
+           <Text style={{fontSize:15,color:'#fff',fontWeight:'bold',paddingTop:10,paddingBottom:15,}}>这家伙很懒，什么都没写~</Text>
+         </View>
+         <View style={{display:'flex',flexDirection:'column', backgroundColor:'#EEEFF6',}}>
+           <View style={styles.nav}>
+             <View style={styles.navList}>
+               <Text style={{fontSize:20,color:'#3B3C40',fontWeight:'bold',}}>0</Text>
+               <Text style={{fontSize:15,color:'#AFB3BF'}}>获赞</Text>
+             </View>
+             <View style={styles.navList}>
+               <Text style={{fontSize:20,color:'#3B3C40',fontWeight:'bold',}}>0</Text>
+               <Text style={{fontSize:15,color:'#AFB3BF'}}>关注</Text>
+             </View>
+             <View style={styles.navList}>
+               <Text style={{fontSize:20,color:'#3B3C40',fontWeight:'bold',}}>0</Text>
+               <Text style={{fontSize:15,color:'#AFB3BF'}}>粉丝</Text>
+             </View>
+             <View style={styles.navList}>
+               <Image source={require('../image/brush.jpg')}
+                 style={{width:20,height:30,}}/>
+               <Text style={{fontSize:15,color:'#AFB3BF'}}>个人资料</Text>
+             </View>
+           </View>
+           <View style={styles.body}>
+             <View style={{display:'flex',flexDirection:'row',justifyContent:'space-between',alignItems:'center', borderBottomWidth: 1, borderColor:'#AFB3BF', paddingLeft:20, paddingRight:20, paddingTop:10, paddingBottom:10,}}>
+                   <Image source={require('../image/plane.jpg')}
+                          style={{width:30,height:30,}}
+                   />
+               <View style={{display:'flex',flexDirection:'row',flex:1,justifyContent:'space-between',alignItems:'center'}}>
+                 <Text style={{fontSize:15,fontWeight:'bold',color:'#2A2B2F',paddingLeft:20,}}>纸飞机</Text>
+                 <Image source={require('../image/icon.jpg')}
+                        style={{width:20,height:20,}}/>
+               </View>
+
+             </View>
+             <View style={{display:'flex',flexDirection:'row',justifyContent:'space-between',alignItems:'center', borderBottomWidth: 1, borderColor:'#AFB3BF', paddingLeft:20, paddingRight:20, paddingTop:10, paddingBottom:10,}}>
+               <Image source={require('../image/plane.jpg')}
+                      style={{width:30,height:30,}}
+               />
+               <View style={{display:'flex',flexDirection:'row',flex:1,justifyContent:'space-between',alignItems:'center'}}>
+                 <Text style={{fontSize:15,fontWeight:'bold',color:'#2A2B2F',paddingLeft:20,}}>纸飞机</Text>
+                 <Image source={require('../image/icon.jpg')}
+                        style={{width:20,height:20,}}/>
+               </View>
+
+             </View>
+             <View style={{display:'flex',flexDirection:'row',justifyContent:'space-between',alignItems:'center', borderBottomWidth: 1, borderColor:'#AFB3BF', paddingLeft:20, paddingRight:20, paddingTop:10, paddingBottom:10,}}>
+               <Image source={require('../image/plane.jpg')}
+                      style={{width:30,height:30,}}
+               />
+               <View style={{display:'flex',flexDirection:'row',flex:1,justifyContent:'space-between',alignItems:'center'}}>
+                 <Text style={{fontSize:15,fontWeight:'bold',color:'#2A2B2F',paddingLeft:20,}}>纸飞机</Text>
+                 <Image source={require('../image/icon.jpg')}
+                        style={{width:20,height:20,}}/>
+               </View>
+
+             </View>
+             <View style={{display:'flex',flexDirection:'row',justifyContent:'space-between',alignItems:'center', borderBottomWidth: 1, borderColor:'#AFB3BF', paddingLeft:20, paddingRight:20, paddingTop:10, paddingBottom:10,}}>
+               <Image source={require('../image/plane.jpg')}
+                      style={{width:30,height:30,}}
+               />
+               <View style={{display:'flex',flexDirection:'row',flex:1,justifyContent:'space-between',alignItems:'center'}}>
+                 <Text style={{fontSize:15,fontWeight:'bold',color:'#2A2B2F',paddingLeft:20,}}>纸飞机</Text>
+                 <Image source={require('../image/icon.jpg')}
+                        style={{width:20,height:20,}}/>
+               </View>
+
+             </View>
+             <View style={{display:'flex',flexDirection:'row',justifyContent:'space-between',alignItems:'center', borderBottomWidth: 1, borderColor:'#AFB3BF', paddingLeft:20, paddingRight:20, paddingTop:10, paddingBottom:10,}}>
+               <Image source={require('../image/plane.jpg')}
+                      style={{width:30,height:30,}}
+               />
+               <View style={{display:'flex',flexDirection:'row',flex:1,justifyContent:'space-between',alignItems:'center'}}>
+                 <Text style={{fontSize:15,fontWeight:'bold',color:'#2A2B2F',paddingLeft:20,}}>纸飞机</Text>
+                 <Image source={require('../image/icon.jpg')}
+                        style={{width:20,height:20,}}/>
+               </View>
+
+             </View>
+             <View style={{display:'flex',flexDirection:'row',justifyContent:'space-between',alignItems:'center', borderBottomWidth: 1, borderColor:'#AFB3BF', paddingLeft:20, paddingRight:20, paddingTop:10, paddingBottom:10,}}>
+               <Image source={require('../image/plane.jpg')}
+                      style={{width:30,height:30,}}
+               />
+               <View style={{display:'flex',flexDirection:'row',flex:1,justifyContent:'space-between',alignItems:'center'}}>
+                 <Text style={{fontSize:15,fontWeight:'bold',color:'#2A2B2F',paddingLeft:20,}}>纸飞机</Text>
+                 <Image source={require('../image/icon.jpg')}
+                        style={{width:20,height:20,}}/>
+               </View>
+
+             </View>
+             <View style={{display:'flex',flexDirection:'row',justifyContent:'space-between',alignItems:'center', borderBottomWidth: 1, borderColor:'#AFB3BF', paddingLeft:20, paddingRight:20, paddingTop:10, paddingBottom:10,}}>
+               <Image source={require('../image/plane.jpg')}
+                      style={{width:30,height:30,}}
+               />
+               <View style={{display:'flex',flexDirection:'row',flex:1,justifyContent:'space-between',alignItems:'center'}}>
+                 <Text style={{fontSize:15,fontWeight:'bold',color:'#2A2B2F',paddingLeft:20,}}>纸飞机</Text>
+                 <Image source={require('../image/icon.jpg')}
+                        style={{width:20,height:20,}}/>
+               </View>
+
+             </View>
+             <View style={{display:'flex',flexDirection:'row',justifyContent:'space-between',alignItems:'center', borderBottomWidth: 1, borderColor:'#AFB3BF', paddingLeft:20, paddingRight:20, paddingTop:10, paddingBottom:10,}}>
+               <Image source={require('../image/plane.jpg')}
+                      style={{width:30,height:30,}}
+               />
+               <View style={{display:'flex',flexDirection:'row',flex:1,justifyContent:'space-between',alignItems:'center'}}>
+                 <Text style={{fontSize:15,fontWeight:'bold',color:'#2A2B2F',paddingLeft:20,}}>纸飞机</Text>
+                 <Image source={require('../image/icon.jpg')}
+                        style={{width:20,height:20,}}/>
+               </View>
+
+             </View>
+             <View style={{display:'flex',flexDirection:'row',justifyContent:'space-between',alignItems:'center', borderBottomWidth: 1, borderColor:'#AFB3BF', paddingLeft:20, paddingRight:20, paddingTop:10, paddingBottom:10,}}>
+               <Image source={require('../image/plane.jpg')}
+                      style={{width:30,height:30,}}
+               />
+               <View style={{display:'flex',flexDirection:'row',flex:1,justifyContent:'space-between',alignItems:'center'}}>
+                 <Text style={{fontSize:15,fontWeight:'bold',color:'#2A2B2F',paddingLeft:20,}}>纸飞机</Text>
+                 <Image source={require('../image/icon.jpg')}
+                        style={{width:20,height:20,}}/>
+               </View>
+
+             </View>
+           </View>
+         </View>
+       </View>
+      </View>
+      </ScrollView>
+    )
+  }
+}
+
+const styles=StyleSheet.create({
+  body:{
+    display:'flex',
+    flexDirection:'column',
+    backgroundColor:'#fff',
+    marginTop:10,
+
+
+  },
+  nav:{
+    display:'flex',
+    flexDirection:'row',
+    justifyContent:'space-around',
+    alignItems:'center',
+    backgroundColor: '#fff',
+    paddingLeft:20,
+    paddingRight:20,
+    paddingTop:10,
+    paddingBottom:10,
+
+  },
+  navList:{
+    display:'flex',
+    flexDirection:'column',
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  headerName:{
+    display:'flex',
+    flexDirection:'row',
+    justifyContent: 'flex-start',
+    alignItems:'center',
+    marginTop:20,
+
+  },
+  header:{
+    display:'flex',
+    flexDirection:'column',
+    justifyContent:'flex-start',
+    paddingLeft:20,
+    paddingRight:20,
+  },
+  top:{
+    display:'flex',
+    flexDirection: 'column',
+    paddingTop:25,
+    paddingBottom:25,
+    marginTop:-230,
+
+  },
+  container:{
+    display:'flex',
+    flex:1,
+    flexDirection:'column',
+  },
+
+})
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import Register from './app/register';
 import Login from './app/login';
 import Forget from './app/forgetPwd';
 import Search from './app/search';
+import Information from './app/personal-information/home';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Search);
+AppRegistry.registerComponent(appName, () => Information);
